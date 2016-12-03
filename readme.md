8cd30234e57f6eb468b528fb44028a40ac373f70
Suren <surendar.ece@gmail.com>
2016-12-03 19:36:47 +0530

    spring-mvc-weather-app-master
    spring-mvc-weather-app-master

---
+++ readme.md
@@ -0,0 +1,14 @@
+Simple single page web-application.
+
+Demo:
+
+[weather-demo.herokuapp.com/](https://weather-demo.herokuapp.com/)
+
+Technologies: OpenJDK 8, Spring MVC 4.2.5, Ajax, JSON, jQuery 1.12.0, Twitter Bootstrap 3.3.6, JUnit 4.12.
+
+Weather Source: OpenWeatherMap API
+
+Author: Nikolaj Olejnik
+
+![alt tag](http://www.longleafalliance.org/photos/misc/workInProgress.gif/image)
+
---
+++ src/main/java/ua/olejnik/web/MainController.java
@@ -0,0 +1,32 @@
+package main.java.ua.olejnik.web;
+
+import main.java.ua.olejnik.domain.Weather;
+import main.java.ua.olejnik.service.WeatherService;
+
+import org.json.simple.parser.ParseException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+
+    private final WeatherService weatherService = new WeatherService();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getMainPage() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/weather/{city}", method = RequestMethod.POST)
+    public @ResponseBody Weather weather(
+            @PathVariable(value = "city") String city) throws IOException, ParseException {
+        return weatherService.getWeather(city);
+    }
+}
